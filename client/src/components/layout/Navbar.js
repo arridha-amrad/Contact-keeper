19f36d4fcb71d0b528e63f06a2d97648ad6323d9
amrad21 <arridhaamrad@gmail.com>
2019-07-08 14:09:18 +0700

    Ready to working with API

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,44 @@
+import React, {Fragment, useContext} from 'react';
+import {Link} from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+const Navbar = () => {
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logoutUser, user } = authContext;
+
+  const onLogout = () => {
+    logoutUser();
+  }
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!"><i className="fas fa-sign-out-alt"></i>
+        <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </Fragment>
+  );
+
+  return (
+    <div className="navbar bg-primary">
+      <h1>  
+        <Link to="/"><i className="fas fa-id-card-alt"></i> Contact Keeper</Link>
+      </h1>
+      <ul>
+        {isAuthenticated ? authLinks : guestLinks}
+      </ul>
+    </div>
+  )
+}
+
+
+export default Navbar
